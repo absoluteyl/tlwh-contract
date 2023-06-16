cd15911e9422714d900f0ca3ee9c0555f9356a92
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-06-16 14:34:37 +0800

    add sepolia endpoint and .env keys for accessing rpc and deploy
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- script/theLastWatchHist.s.sol
+++ script/theLastWatchHist.s.sol
@@ -5,7 +5,7 @@ import "src/theLastWatchHist.sol";
 
 contract TheLastWatchHistScript is Script {
   function run() external {
-    vm.startBroadcast(vm.envUint("WALLET_PRIVATE_KEY"));
+    vm.startBroadcast(vm.envUint("WALLET_PRIVATE"));
 
     TheLastWatchHist tLWH = new TheLastWatchHist();
     tLWH.initialize();
