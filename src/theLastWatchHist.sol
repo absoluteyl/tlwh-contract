054e9f21f60954c525ef84ae38b4b9909201e0a3
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-06-22 12:45:02 +0800

    import ERC721Upgradable and ERC721EnumberableUpgradable
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- src/theLastWatchHist.sol
+++ src/theLastWatchHist.sol
@@ -1,10 +1,14 @@
 pragma solidity ^0.8.19;
 
+import "openzeppelin/token/ERC721/ERC721Upgradeable.sol";
+import "openzeppelin/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import "openzeppelin/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 
-contract TheLastWatchHist is ERC721URIStorageUpgradeable {
+contract TheLastWatchHist is ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable {
   function initialize(string memory name, string memory symbol) external initializer {
     __ERC721_init(name, symbol);
+    __ERC721Enumerable_init();
+    __ERC721URIStorage_init();
   }
 
   function mint(uint256 tokenId, string memory tokenURI) external {
@@ -19,5 +23,38 @@ contract TheLastWatchHist is ERC721URIStorageUpgradeable {
     require(owner == msg.sender, "TheLastWatchHist: only owner can burn this token");
 
     _burn(tokenId);
+  }
+
+  // The following functions are overrides required by Solidity to solve inheritance ambiguity
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+    internal
+    override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+  {
+    super._beforeTokenTransfer(from, to, tokenId, batchSize);
+  }
+
+  function _burn(uint256 tokenId)
+    internal
+    override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+  {
+    super._burn(tokenId);
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+    returns (string memory)
+  {
+    return super.tokenURI(tokenId);
+  }
+
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable)
+    returns (bool)
+  {
+    return super.supportsInterface(interfaceId);
   }
 }
