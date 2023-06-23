b9d460f4ab98c454c68728f6971a5582bc3f576b
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-06-23 10:45:00 +0800

    replace proxy code with oz ERC1967Proxy
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- src/tlwhProxy.sol
+++ src/tlwhProxy.sol
@@ -1,34 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract TLWHProxy {
-  constructor(bytes memory constructData, address contractLogic) {
-    assembly { // solium-disable-line
-      sstore(
-        0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, // keccak256("PROXIABLE")
-        contractLogic
-      )
-    }
-    (bool success, ) = contractLogic.delegatecall(constructData); // solium-disable-line
-    require(success, "Construction failed");
-  }
+import "openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-  fallback() external payable {
-    assembly { // solium-disable-line
-      let contractLogic := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
-      calldatacopy(0x0, 0x0, calldatasize())
-      let success := delegatecall(sub(gas(), 10000), contractLogic, 0x0, calldatasize(), 0, 0)
-      let retSz := returndatasize()
-      returndatacopy(0, 0, retSz)
-      switch success
-      case 0 {
-        revert(0, retSz)
-      }
-      default {
-        return(0, retSz)
-      }
-    }
-  }
-
-    receive() external payable {}
+contract TLWHProxy is ERC1967Proxy {
+  constructor(
+    address _logic, bytes memory _initCode
+  ) ERC1967Proxy(_logic, _initCode) {}
 }
