db3b1e3348dad696b42e7cf764039e6522089e00
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-06-23 11:20:10 +0800

    add proxy deployment step in script
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- script/tlwh.s.sol
+++ script/tlwh.s.sol
@@ -2,13 +2,17 @@ pragma solidity 0.8.19;
 
 import "forge-std/Script.sol";
 import "src/tlwhToken.sol";
+import "src/tlwhProxy.sol";
 
 contract TLWHScript is Script {
   function run() external {
     vm.startBroadcast(vm.envUint("WALLET_PRIVATE"));
 
-    TLWHToken tLWH = new TLWHToken();
-    tLWH.initialize("TheLastWatchHist", "TLWH");
+    TLWHToken token = new TLWHToken();
+    new TLWHProxy(
+      address(token),
+      abi.encodeWithSignature("initialize(string,string)", "TheLastWatchHist", "TLWH")
+    );
 
     vm.stopBroadcast();
   }
