e30b258c2fa6a26a9158b48938734c57eef48e31
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-06-23 09:46:34 +0800

    update test to use proxy
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- test/theLastWatchHist.t.sol
+++ test/theLastWatchHist.t.sol
@@ -2,9 +2,13 @@ pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
 import "src/theLastWatchHist.sol";
+import "src/theLastWatchHistProxy.sol";
 
 contract TheLastWatchHistTest is Test {
+  TheLastWatchHistProxy public proxy;
+
   TheLastWatchHist public tLWH;
+  TheLastWatchHist public proxiedTLWH;
 
   address public user1;
   address public user2;
@@ -14,7 +18,11 @@ contract TheLastWatchHistTest is Test {
 
   function setUp() public {
     tLWH = new TheLastWatchHist();
-    tLWH.initialize("TheLastWatchHist", "TLWH");
+    proxy = new TheLastWatchHistProxy(
+      abi.encodeWithSignature("initialize(string,string)", "TheLastWatchHist", "TLWH"),
+      address(tLWH)
+    );
+    proxiedTLWH = TheLastWatchHist(address(proxy));
 
     user1 = makeAddr("user1");
     user2 = makeAddr("user2");
@@ -26,72 +34,72 @@ contract TheLastWatchHistTest is Test {
   }
 
   function testDeploy() public {
-    assertEq(tLWH.name(), "TheLastWatchHist");
-    assertEq(tLWH.symbol(), "TLWH");
+    assertEq(proxiedTLWH.name(), "TheLastWatchHist");
+    assertEq(proxiedTLWH.symbol(), "TLWH");
   }
 
   function testMintBurn() public {
     // Mint
     vm.prank(user1);
-    tLWH.mint(ipfsId1);
+    proxiedTLWH.mint(ipfsId1);
 
-    assertEq(tLWH.ownerOf(0), user1);
-    assertEq(tLWH.tokenURI(0), string.concat(ipfsHost, ipfsId1));
+    assertEq(proxiedTLWH.ownerOf(0), user1);
+    assertEq(proxiedTLWH.tokenURI(0), string.concat(ipfsHost, ipfsId1));
 
     // Burn - only owner can burn
     vm.expectRevert("ERC721: caller is not token owner or approved");
-    tLWH.burn(0);
+    proxiedTLWH.burn(0);
 
     vm.prank(user1);
-    tLWH.burn(0);
+    proxiedTLWH.burn(0);
 
-    assertEq(tLWH.balanceOf(user1), 0);
+    assertEq(proxiedTLWH.balanceOf(user1), 0);
   }
 
   function testOnlyOneMintPerAddress() public {
     // First Mint
     vm.prank(user1);
-    tLWH.mint(ipfsId1);
+    proxiedTLWH.mint(ipfsId1);
 
-    assertEq(tLWH.balanceOf(user1), 1);
+    assertEq(proxiedTLWH.balanceOf(user1), 1);
 
     // Second Mint
     vm.prank(user1);
     vm.expectRevert("TheLastWatchHist: only one mint is allowed per address");
-    tLWH.mint(ipfsId1);
+    proxiedTLWH.mint(ipfsId1);
 
-    assertEq(tLWH.balanceOf(user1), 1);
+    assertEq(proxiedTLWH.balanceOf(user1), 1);
   }
 
   function testTokenIdAutoIncrement() public {
     // User1 mints
     vm.prank(user1);
-    tLWH.mint(ipfsId1);
+    proxiedTLWH.mint(ipfsId1);
 
-    assertEq(tLWH.ownerOf(0), user1);
-    assertEq(tLWH.tokenURI(0), string.concat(ipfsHost, ipfsId1));
+    assertEq(proxiedTLWH.ownerOf(0), user1);
+    assertEq(proxiedTLWH.tokenURI(0), string.concat(ipfsHost, ipfsId1));
 
     // User2 mints
     vm.prank(user2);
-    tLWH.mint(ipfsId2);
+    proxiedTLWH.mint(ipfsId2);
 
-    assertEq(tLWH.ownerOf(1), user2);
-    assertEq(tLWH.tokenURI(1), string.concat(ipfsHost, ipfsId2));
+    assertEq(proxiedTLWH.ownerOf(1), user2);
+    assertEq(proxiedTLWH.tokenURI(1), string.concat(ipfsHost, ipfsId2));
   }
 
   function testTransferNotAllowed() public {
     // Mint
     vm.prank(user1);
-    tLWH.mint(ipfsId1);
+    proxiedTLWH.mint(ipfsId1);
 
-    assertEq(tLWH.balanceOf(user1), 1);
+    assertEq(proxiedTLWH.balanceOf(user1), 1);
 
     // Transfer
     vm.prank(user1);
     vm.expectRevert("TheLastWatchHist: Token is soul-bound and not transferable");
-    tLWH.transferFrom(user1, user2, 0);
+    proxiedTLWH.transferFrom(user1, user2, 0);
 
-    assertEq(tLWH.balanceOf(user1), 1);
-    assertEq(tLWH.balanceOf(user2), 0);
+    assertEq(proxiedTLWH.balanceOf(user1), 1);
+    assertEq(proxiedTLWH.balanceOf(user2), 0);
   }
 }
